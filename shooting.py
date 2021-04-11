abc88473a98889a7d913b8d07171207b8e58f7fb
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2021-04-11 17:20:25 +0200

    Update shooting.py

--- shooting.py
+++ shooting.py
@@ -8,11 +8,11 @@ x(1)=x1
 si consiglia l'esecuzione a celle per avere un'idea del valore iniziale da dare
 we recommend the execution in cells to get an idea of ​​the initial value to be given
 '''
-xi=0       #estremo sinistro dell'intervallo
-xf=1       #estremo desto dell'intervallo
-N=1000     #numero di punti
-x0=4       #valore all'estremo sinistro
-x1=1       #valore che vogliamo assumi la soluzione nell'estemo destro
+xi=0        #estremo sinistro dell'intervallo
+xf=1        #estremo desto dell'intervallo
+N=1000      #numero di punti
+x0=4        #valore all'estremo sinistro
+x1=1        #valore che vogliamo assumi la soluzione nell'estemo destro
 tau=1.0e-10 #tolleranza
 
 
@@ -84,12 +84,12 @@ v0, y1=SH(xf, N, x0, -40, 0.1, x1, tau)
 v1, y2=SH(xf, N, x0, -10, 0.1, x1, tau)
 
 plt.figure(2)
-plt.title('Soluzioni per due diverse condizioni inziali che danno la stessa condizione al contorno \n x1(1)-1=%e, x2(1)-1=%e ' %(y1[-1]-1, y2[-1]-1), fontsize=20)
+plt.title('Soluzioni per due diverse condizioni inziali che danno la stessa condizione al contorno \n $x_1(t_f)$-x1=%e, $x_2(t_f)$-x1=%e ' %(y1[-1]-x1, y2[-1]-x1), fontsize=20)
 
 plt.ylabel('x(t)', fontsize=15)
 plt.xlabel('t', fontsize=15)
 plt.grid()
-plt.plot(t1, y1, 'k', label='x1(t), $\dot{x}(t=0)$=%f' %v0)
-plt.plot(t1, y2, 'b', label='x2(t), $\dot{x}(t=0)$=%f' %v1)
+plt.plot(t1, y1, 'k', label='$x_1(t)$, $\dot{x}(t=0)$=%f' %v0)
+plt.plot(t1, y2, 'b', label='$x_2(t)$, $\dot{x}(t=0)$=%f' %v1)
 plt.legend(loc='best')
 plt.show()
