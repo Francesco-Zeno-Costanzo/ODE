558c93dd3d341cf9c6c0710e0167b59ab3769681
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2023-09-06 01:01:55 +0200

    BVP via relaxation method

--- relax.py
+++ relax.py
@@ -2,8 +2,49 @@ import numpy as np
 from scipy.sparse import diags
 import  matplotlib.pyplot as plt
 
-def relax(f, y0, y1, init, N, h, max_iter):
+#=================================================================================
+# Function for the solution of boundary value problem via relaxation
+#=================================================================================
 
+def relax(f, y0, y1, x, init, args=(), tol=1e-8, max_iter=100, dense_output=False):
+    '''
+    Implementation of relaxation method for ODE 2pt-BVP.
+    The equation must be in the form: y''(x) = f(x, y, y')
+    Using y[0]=y0 and y[-1]=y1, the discretizzation lead to:
+    
+    y[i-1] - 2y[i] + y[i+1]                 y[i-1] - y[i+1]
+    ----------------------- = f(x[i], y[i], --------------- )
+              h**2                                2h
+    
+    Parameters
+    ----------
+    f : callable
+        A vector function of differential equation like: y'' = f
+    y0, y1 : float
+        required value of solution at boundary
+    x : 1darray
+        array of position, or time, independent variable
+    init : 1darray 
+        Initial guess.
+    args : tuple, optional
+        Extra arguments passed to f
+    tol :float, optional, default 1e-8
+        required tollerance
+    max_iter : int, optional, default 100
+        after max_it iteration the code stop raising an exception
+    dense_output : bool, optional, default False
+        true for full and number of iteration
+    
+    Return
+    ------
+    yo : 1darray
+        solution of differential equation
+        if dense_outupt=True all iteration are returned
+        in a matrix called Y and also the number of iteration
+    '''
+    # parameter of discretizzation
+    N = len(x)
+    h = np.diff(x)[0]
     # second derivative matrix
     d2 = diags([1, -2, 1], [-1, 0, 1], shape=(N, N)).toarray()
     d2 = d2/h**2
@@ -12,26 +53,69 @@ def relax(f, y0, y1, init, N, h, max_iter):
     yb = np.array(yb)
     # init guess from imput
     yo = init
-
-    for i in range(max_iter):
+    # interation count
+    it = 0
+    #for full output
+    Y = []
+    if dense_output : Y.append(yo)
+    
+    while True:
+        # for jacobian computation
         df = np.zeros(d2.shape)
         s  = np.zeros(N)
 
         for i in range(N):
             s[i] = 1
             yr, yl = yo + h*s, yo - h*s
-            df[i, :] = (f(x, yr, h, g, o02) - f(x, yl, h, g, o02) )/(2*h)
+            df[i, :] = (f(x, yr, h, *args) - f(x, yl, h, *args) )/(2*h)
             s[:] = 0
 
         yn = yo - np.linalg.solve(d2-df, d2@yo - f(x, yo, h, g, o02) + yb)
-        # conti distanza?
+        # residual
+        R = np.sqrt(np.sum((yn-yo)**2))
+        if R < tol:
+            yo = yn
+            break
+            
+        if it > max_iter:
+            raise Exception("to many iteration")
+
+        #update
         yo = yn
-    return yo
+        it = it + 1
+        if dense_output : Y.append(yo)
+    
+    if dense_output:
+        return Y, it
+    else:
+        return yo
 
-
+#=================================================================================
+# RHS of differential equations y'' = f
+#=================================================================================
 
 def f(t, y, h, g, o02):
-
+    '''
+    RHS of differential equations y'' = f
+    f can be a function non only of y but also y' so
+    we use a second order approximation to compute y'
+    
+    Parameter
+    ---------
+    t : 1darray
+        independent variable
+    y : 1darray
+        solution or guess of solution
+    h : float
+        step's size for derivative computation
+    g, o02 : float
+        parameter of our differential equation
+    
+    Return
+    ------
+    y_ddot : float
+        RHS of equation computed on a grid
+    '''
     y_dot   = (y[2:] - y[:-2])/(2*h)              # second order derivative
     y_dot_0 = (- 3*y[0]  + 4*y[1]  - y[2] )/(2*h) # second order derivative on left  bound
     y_dot_n = (  3*y[-1] - 4*y[-2] + y[-3])/(2*h) # second order derivative on right bound
@@ -44,21 +128,26 @@ def f(t, y, h, g, o02):
 
     return y_ddot
 
+#=================================================================================
+# Main code and plot
+#=================================================================================
 
-g   = 0.3                    # damping factor
-o02 = 1                      # proper frequency squared
-xi  = 0                      # left end of the interval
-N   = 1000                   # number of points
-y0  = 1
-xf = 5
-y1 = 0.1862
+g   = 0.3    # damping factor
+o02 = 1      # proper frequency squared
+xi  = 0      # left  end of the interval
+xf  = 5      # right end of the interval
+N   = 1000   # number of points
+y0  = 1      # boundary condition on xi
+y1  = 0.1862 # boundary condition on xf
 
 x = np.linspace(xi, xf, N)
-h = np.diff(x)[0]
-y = (x - xi) * (y1 - y0)/(xf - xi) + y0
+y = (x - xi) * (y1 - y0)/(xf - xi) + y0 # linear guess
 
-y = relax(f, y0, y1, y, N, h, 50)
+Y, n = relax(f, y0, y1, x, y,args=(g, o02), dense_output=True)
+print(f"{n} iterations required")
 
-plt.plot(x, y)
+for y in Y:
+    plt.plot(x, y)
 
+plt.grid()
 plt.show()
